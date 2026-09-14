6731a6a843fffba390a99ec74a95319b6804663d
uschinsu-spec <uschinsu@gmail.com>
2026-09-14 15:32:04 +0700

    Add rigged player asset config

---
+++ assets/characters/player-rig-config.js
@@ -0,0 +1,7 @@
+window.PLAYER_RIG_CONFIG={
+  characterUrl:'https://raw.githubusercontent.com/Seyamalam/blood-league-kickoff/aa02a4e6d8337a0604d2da131bcbbeb1f01badf0/public/assets/vendor/quaternius/night-striker.glb',
+  animationUrl:'https://raw.githubusercontent.com/Seyamalam/blood-league-kickoff/aa02a4e6d8337a0604d2da131bcbbeb1f01badf0/public/assets/vendor/quaternius/universal-animation-library.glb',
+  scale:{x:.92,y:.82,z:.92},
+  headScale:1.28,
+  sockets:{head:'Head',rightHand:'hand_r',leftHand:'hand_l',rightFoot:'foot_r',leftFoot:'foot_l'}
+};
