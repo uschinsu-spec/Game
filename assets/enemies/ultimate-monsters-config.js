a9b7b6634958827cc28b9c349ff07ccd709347e9
uschinsu-spec <uschinsu@gmail.com>
2026-09-14 15:49:19 +0700

    Add Ultimate Monsters enemy catalog

---
+++ assets/enemies/ultimate-monsters-config.js
@@ -0,0 +1,21 @@
+window.ULTIMATE_MONSTERS_CONFIG={
+  source:'Quaternius Ultimate Monsters',
+  license:'CC0-1.0',
+  official:'https://quaternius.com/packs/ultimatemonsters.html',
+  mirrorCommit:'7380bcd7005e99f1ad690d5dd4015f215d8673bf',
+  root:'https://raw.githubusercontent.com/brornski/ashenhold-dragonfall/7380bcd7005e99f1ad690d5dd4015f215d8673bf/assets/models/quaternius-monsters/',
+  catalog:[
+    {id:'frog',file:'frog.gltf',name:'Linh Oa',role:'mob',scale:1.05,y:-.78},
+    {id:'birb',file:'birb.gltf',name:'Yêu Điểu',role:'mob',scale:1.02,y:-.72},
+    {id:'fish',file:'fish.gltf',name:'Thủy Yêu',role:'mob',scale:1.08,y:-.7},
+    {id:'cactoro',file:'cactoro.gltf',name:'Mộc Linh',role:'mob',scale:.98,y:-.78},
+    {id:'alien',file:'alien.gltf',name:'Dị Linh',role:'mob',scale:1.0,y:-.78},
+    {id:'monkroose',file:'monkroose.gltf',name:'Sơn Yêu',role:'elite',scale:1.08,y:-.78},
+    {id:'tribal',file:'tribal.gltf',name:'Man Yêu',role:'elite',scale:1.08,y:-.78},
+    {id:'orc',file:'orc.gltf',name:'Cự Nha Yêu',role:'elite',scale:1.08,y:-.78},
+    {id:'orc-skull',file:'orc-skull.gltf',name:'Khô Lâu Yêu Vương',role:'boss',scale:1.12,y:-.78},
+    {id:'demon',file:'demon.gltf',name:'Xích Ma Vương',role:'boss',scale:1.08,y:-.78},
+    {id:'blue-demon',file:'blue-demon.gltf',name:'U Lam Ma Vương',role:'boss',scale:1.08,y:-.78},
+    {id:'yeti',file:'yeti.gltf',name:'Hàn Sơn Yêu Vương',role:'boss',scale:1.08,y:-.78}
+  ]
+};
